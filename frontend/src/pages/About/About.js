0fe04c534767931e7d8ebeb3af9ae12a3b56f0f3
Nehamk21 <nmkamtalwar@gmail.com>
2024-05-30 19:39:32 +0530

    first commit

---
+++ frontend/src/pages/About/About.js
@@ -0,0 +1,65 @@
+import React,{useRef,useEffect, useState} from 'react'
+import { Helmet } from 'react-helmet';
+import {Image} from '@chakra-ui/react'
+import HashLoader from "react-spinners/HashLoader";
+import './aboutcss.css'
+const About = () => {
+    const Line = useRef(null);
+    const text = useRef(null);
+    useEffect(() => {
+                setTimeout(() =>{
+        Line.current.classList.add('lineon')
+        text.current.classList.add('titleon');
+        },5)
+
+
+        return () => {
+
+        }
+    },[])
+    return (
+        
+
+        
+        <>
+        <Helmet>
+            <title>About</title>
+        </Helmet>
+
+
+            <div className='headingA'>
+                <div className = 'line' ref={Line}>
+                </div>
+                <h1 className ='title' ref={text}>About Us</h1>
+            </div>
+            <div className='Content1'>
+                <div className = 'text'>
+                    <h1>
+                        Why?
+                    </h1>
+                <p>Our e-commerce platform was born out of a shared passion for fashion and accessories. We noticed a gap in the market for affordable yet stylish accessories and envisioned a platform that not only addresses this need but also provides an exceptional shopping experience. With a commitment to quality, we carefully curate our selection to offer you the best jewelry, handbags, and fashion accessories available. Our team is dedicated to ensuring that every interaction with our platform is smooth and enjoyable. Whether you're a seasoned fashionista or new to this world, we welcome you to join our growing community. Together, let's explore and celebrate the world of fashion and accessories!</p>
+                </div>
+               
+                   <div className ='imagecontainer'>
+                    <div className = 'Imageabt'>
+                    <Image className='mImage' boxSize = '400px' objectFit="cover" src='https://www.insightssuccess.in/wp-content/uploads/2020/12/785054-ecommerce-istock-020119.jpg' alt="Segun Adebayo"/>
+                    </div>
+                   </div>
+            </div>
+            {/* <div className ='Content2'>
+                   
+                   <div className ='imagecontainer'>
+                    <div className = 'Imageabt'>
+                    <Image className='mImage' boxSize = '400px' objectFit="cover" src='https://images.unsplash.com/photo-1614038276039-667c23bc32fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=389&q=80' alt="Segun Adebayo"/>
+                    </div>
+                   </div>
+                   <div className = 'text'>
+                    
+                </div>
+            </div> */}
+        </>
+    
+    )
+}
+
+export default About
